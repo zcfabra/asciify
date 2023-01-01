66c82a4830e72c584042d99f3a03e25b30dd4001
zcfabra <christianwgnam@gmail.com>
2022-12-31 19:36:05 -0500

    file upload flow

--- web/src/app.rs
+++ web/src/app.rs
@@ -1,12 +1,33 @@
 use yew::prelude::*;
-
+use web_sys::{HtmlInputElement, Response};
 #[function_component(App)]
 pub fn app() -> Html {
+
+
+
+    let input_ref = use_node_ref(); 
+    let onchange = {
+        let input_ref = input_ref.clone();
+
+        Callback::from(  move |_|{
+            let input = input_ref.cast::<HtmlInputElement>();
+            if let Some(input) = input {
+                web_sys::console::log_1(&"Hi".to_string().into());
+                let file = input.files().unwrap().item(0).unwrap();
+                web_sys::console::log_1(&file);
+                let contents = file.into();
+
+
+
+            }
+        })
+    };
     html! {
-        <main>
-            <img class="logo" src="https://yew.rs/img/logo.png" alt="Yew logo" />
-            <h1>{ "Hello World!" }</h1>
-            <span class="subtitle">{ "from Yew with " }<i class="heart" /></span>
+        <main class="w-full h-full bg-black text-green-500 flex flex-col items-center">
+            <h1>{ "a s c i i f y" }</h1>
+            <div class="w-10/12 lg:w-5/12 h-4/6 rounded-xl border border-green-500">
+                <input ref={input_ref} type="file" {onchange}/> 
+            </div>
         </main>
     }
 }
